8cbc5de9672954efc4ba2b929965529f9524128d
zMityxxxx <97694458+zMityxxxx@users.noreply.github.com>
2022-01-18 20:32:20 +0100

    Update Boutique.php
    test

--- src/Wanny/Nephtys/Commands/Boutique.php
+++ src/Wanny/Nephtys/Commands/Boutique.php
@@ -26,13 +26,13 @@ class Boutique extends Command {
     public function execute(CommandSender $sender, string $commandLabel, array $args)
     {
         if ($sender instanceof NephtysPlayer) {
-            $form = new SimpleForm("Boutique", "Que voulez-vous acheter");
-            $form->addButton(new Button("Acheter un slot d'enderchest", null, function (NephtysPlayer $player){$player->sendForm(new SlotShop());}));
-            $form->addButton(new Button("Acheter une clef de box", null, function (NephtysPlayer $player){$player->sendForm(new ClefShop());}));
-            $form->addButton(new Button("Acheter un tag", null, function (NephtysPlayer $player){$player->sendForm(new TagsShop());}));
-            $form->addButton(new Button("Acheter un grade", null, function (NephtysPlayer $player){$player->sendForm(new RankShop());}));
-            $form->addButton(new Button("Acheter un kit", null, function (NephtysPlayer $player){$player->sendMessage("En développement");}));
-            $form->addButton(new Button("Acheter une commande", null, function (NephtysPlayer $player){$player->sendMessage("En développement");}));
+            $form = new SimpleForm("§8Boutique", "§9§l»§r§7 Bienvenue dans la boutique, choisissez la catégorie qui vous plaît");
+            $form->addButton(new Button("Acheter un slot \n§9Enderchest", null, function (NephtysPlayer $player){$player->sendForm(new SlotShop());}));
+            $form->addButton(new Button("Acheter une \n§9Clef de box", null, function (NephtysPlayer $player){$player->sendForm(new ClefShop());}));
+            $form->addButton(new Button("Acheter un\n§9Tag", null, function (NephtysPlayer $player){$player->sendForm(new TagsShop());}));
+            $form->addButton(new Button("Acheter un\n§9Grade", null, function (NephtysPlayer $player){$player->sendForm(new RankShop());}));
+            $form->addButton(new Button("Acheter un\n§9Kit", null, function (NephtysPlayer $player){$player->sendMessage("En développement");}));
+            $form->addButton(new Button("Acheter une\n§9Commande", null, function (NephtysPlayer $player){$player->sendMessage("En développement");}));
             $sender->sendForm($form);
         }
     }
